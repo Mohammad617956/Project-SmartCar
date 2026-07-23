b5fcf2d16b168dbc195f0701ff85261322bdedc5
Mohammad617956 <0195651@schoolsnet.act.edu.au>
2026-07-23 10:41:39 +1000

    smart car

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,40 @@
 #include <ultrasonic.h>
 #include <ESP32Servo.h>
 
+/*
+myCar.Move(Foward,255);
+myCar.Move(Backward, 255);
+myCar.Move(Clockwise,255);
+myCar.Move(Anticlockwise,255)
+myCar.Move(Move-Left,255)
+myCar.Move(Move_Right,255)
+*/
+
+
 vehicle myCar;
 
-
+#define leftLED 2
+#define rightLED 12
+#define Anticlockwise Contrarotate
 void setup()
 {
- 
+ Serial.begin(115200);
+ pinMode(leftLED, OUTPUT);
+ pinMode(rightLED, OUTPUT);
+
+myCar.Init();
+
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+digitalWrite(leftLED, HIGH);
+digitalWrite(rightLED, LOW);
+//myCar.Move(Move_Left, 255);
+delay(1000);
 
-  
+digitalWrite(leftLED, LOW);
+digitalWrite(rightLED, HIGH);
+//myCar.Move(Move_Right , 255);
+delay(1000);
 }
